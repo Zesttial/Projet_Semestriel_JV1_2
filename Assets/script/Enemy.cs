6b5fd58941e86b5348c7a2b38271daa1fbb8b27e
Matheo_psst <matheopousset49@gmail.com>
2026-04-30 17:52:47 +0200

    update_7_30/04/2026

--- Assets/script/Enemy.cs
+++ Assets/script/Enemy.cs
@@ -1,4 +1,5 @@
 using System.Security.Cryptography;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Splines;
 
@@ -9,16 +10,126 @@ public class ennemy1 : MonoBehaviour
     public SplineAnimate mySplineAnimate;
     public HealthManager myHpManager;
     public int hpMax;
+
+    public Transform rayCast;
+    public LayerMask raycastMask;
+    public float rayCastLenght;
+    public float attackDistance;
+    public float moveSpeed;
+    public float timer;
+
+    private RaycastHit2D hit;
+    private GameObject target;
+    private Animator anim;
+    private float distance;
+    private bool attackMode;
+    private bool inRange;
+    private bool cooling;
+    private float intTimer;
+
     void Start()
     {
         myHpManager.ChangeHP(hpMax);
-        mySplineAnimate.Container = FindFirstObjectByType<SplineContainer>();
-        mySplineAnimate.Play();
+        // mySplineAnimate.Container = FindFirstObjectByType<SplineContainer>();
+        //mySplineAnimate.Play();
     }
 
-    // Update is called once per frame
+    void Awake()
+    {
+        intTimer = timer;
+        anim = GetComponent<Animator>();
+    }
     void Update()
     {
+        if (inRange)
+        {
+            hit = Physics2D.Raycast(rayCast.position, Vector2.left, rayCastLenght, raycastMask);
+            RaycastDebugger();
+        }
+
+        //quand joueur détecté
+        if (hit.collider != null)
+        {
+            EnemyLogic();
+        }
+        else if (hit.collider == null)
+        {
+            inRange = false;
+        }
+
+        if (inRange == false)
+        {
+            anim.SetBool("canWalk", false);
+            StopAttack();
+        }
 
     }
+    void OnTriggerEnter2D(Collider2D trig)
+    {
+        if (trig.gameObject.tag == "player")
+        {
+            target = trig.gameObject;
+            inRange = true;
+        }
+    }
+
+    void EnemyLogic()
+    {
+        distance = Vector2.Distance(transform.position, target.transform.position);
+
+        if (distance > attackDistance)
+        {
+            Move();
+            StopAttack();
+        }
+
+        else if (attackDistance >= distance && cooling == false)
+        {
+            Attack();
+        }
+
+        if (cooling)
+        {
+            anim.SetBool("Attack", false);
+        }
+
+    }
+    void Move()
+        {
+            anim.SetBool("canWalk",true);
+            if(! anim.GetCurrentAnimatorStateInfo(0).IsName("Skel_attack"))
+            {
+                Vector2 targetPosition = new Vector2(target.transform.position.x, transform.position.y);
+
+                transform.position = Vector2.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
+            }
+    }
+    void Attack ()
+    {
+            timer = intTimer;
+            attackMode = true;
+
+            anim.SetBool("canWalk", false );
+            anim.SetBool("Attack", true);
+    }
+    void StopAttack()
+        {
+            cooling = false;
+            attackMode = false;
+            anim.SetBool("Attack", false);
+    }
+        
+        
+        void RaycastDebugger()
+        {
+            if (distance > attackDistance)
+            {
+                Debug.DrawRay(rayCast.position, Vector2.left * rayCastLenght, Color.red);
+            }
+            else if (attackDistance > distance)
+            {
+                Debug.DrawRay(rayCast.position, Vector2.left * rayCastLenght, Color.green);
+            }
+        }
+
 }
