f6875bf77d074196a39175e1d96a61e167e5c71b
Zesttial <a.borona@rns.ecolescreatives.com>
2026-05-20 23:24:01 +0200

    Update_20/05/2026/23h

--- Assets/script/Player/move_test.cs
+++ Assets/script/Player/move_test.cs
@@ -9,6 +9,7 @@ public class move_test : MonoBehaviour
     private Animator myAnimator;
     private bool isGrounded;
     private bool isAttack;
+    private bool isBox;
     private float AttackTime;
     private float timeAfterAttackingAnimation;
 
@@ -23,7 +24,7 @@ public class move_test : MonoBehaviour
         float moveInput = Input.GetAxis("Horizontal");
         Rb.linearVelocity = new Vector2(moveInput* Vspeed, Rb.linearVelocity.y);
 
-            if(Input.GetKeyDown(KeyCode.UpArrow) && isGrounded == true || Input.GetKey(KeyCode.Space) && isGrounded ==true)
+            if(Input.GetKeyDown(KeyCode.UpArrow) && isGrounded == true || Input.GetKey(KeyCode.Space) && isGrounded ==true || Input.GetKey(KeyCode.UpArrow) && isBox == true || Input.GetKey(KeyCode.Space) && isBox == true)
             {
                 Rb.linearVelocity = new Vector2(Rb.linearVelocity.x, jumpforce);
 
@@ -31,7 +32,7 @@ public class move_test : MonoBehaviour
 
         float speed = Mathf.Abs(Rb.linearVelocity.x);
         myAnimator.SetFloat("Speed", speed);
-        myAnimator.SetBool("Ground", isGrounded);
+        myAnimator.SetBool("Ground", isGrounded || isBox);
         if (moveInput !=0)
         {
             transform.localScale = new Vector3(Mathf.Sign(moveInput), 1, 1);
@@ -63,12 +64,20 @@ public class move_test : MonoBehaviour
         {
             isGrounded = true;
         }
+        if (collision.gameObject.CompareTag("Box"))
+        {
+            isBox = true;
+        }
     }
     private void OnCollisionExit2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Ground"))
         {
             isGrounded = false;
+        }
+        if (collision.gameObject.CompareTag("Box"))
+        {
+            isBox = false;
         }
     }
 
